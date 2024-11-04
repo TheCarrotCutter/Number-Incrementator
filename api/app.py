ea4e17b6bf2950468c58ce9a825fffaada7309a9
TheCarrotCutter <masonthedecker@gmail.com>
2024-11-03 19:22:11 -0500

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,17 +1,9 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 
-app = Flask(__name__)
-
-@app.route('/process_data', methods=['POST'])
-def process_data():
-    data = request.get_json()
-    name = data.get('name')
-    message = f"Welcome, {name}!"  # Customize your message
-    return jsonify({'message': message})
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)API
+# No app.run() line needed here
