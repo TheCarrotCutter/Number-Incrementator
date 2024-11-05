2243de49dc21c080db40c3883d8d544652973130
TheCarrotCutter <masonthedecker@gmail.com>
2024-11-04 20:45:43 -0500

    Update app.py

--- api/app.py
+++ api/app.py
@@ -5,5 +5,3 @@ app = Flask(__name__, static_folder='api/static', template_folder='api/templates
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# No app.run() line needed here
