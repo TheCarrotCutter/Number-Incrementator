c396bd4e6dba2f933906a5a72e5eddb7488c214f
TheCarrotCutter <masonthedecker@gmail.com>
2024-11-05 15:13:28 -0500

    Update script.js

--- api/static/script.js
+++ api/static/script.js
@@ -9,7 +9,13 @@ function increment() {
 function update() {
     document.getElementById('counter').textContent = number;
     localStorage.setItem('counter', number); // Save the current number in localStorage
-}
+
+
+    const button = document.getElementById('Increment_Upgrade');  // Get the button element
+    if (number < 100) {
+        button.disabled = true
+}    else    {
+        button.disabled = false
 
 document.addEventListener('DOMContentLoaded', function() {
     number = parseInt(localStorage.getItem('counter')) || 0; // Default to 0 if no value exists in localStorage
