f5eac824e3111d3cd0142a9e5728d0d387c9aaf2
TheCarrotCutter <masonthedecker@gmail.com>
2024-11-07 12:18:36 -0500

    Update script.js

--- api/static/script.js
+++ api/static/script.js
@@ -1,7 +1,9 @@
 // Load the stored counter value from localStorage, default to 0 if not found
-let number = 0;
-let increment_ammount = 1;
-let total = 0;
+let number = parseInt(localStorage.getItem('number_save')) || 0; 
+
+let increment_ammount = parseInt(localStorage.getItem('increment_ammount_save')) || 1; 
+
+let total = parseInt(localStorage.getItem('total_save')) || 0;
 
 // Function to increment the counter
 function increment() {
