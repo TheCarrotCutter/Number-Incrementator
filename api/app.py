7d3413dcd8c2ba5e3535b7a9d505d1e925435ae4
TheCarrotCutter <masonthedecker@gmail.com>
2024-11-09 07:37:23 -0500

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,22 +1,7 @@
-import os
-from flask import Flask, render_template, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import Flask, render_template
 
-# Initialize Flask app
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-# Database setup
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_PUBLIC_URL')  # Make sure this matches your .env variable
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Initialize SQLAlchemy
-db = SQLAlchemy(app)
-
-# Initialize Flask-Migrate
-migrate = Migrate(app, db)
-
-# Define a simple route
 @app.route('/')
 def index():
     return render_template('index.html')
