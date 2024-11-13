18653adb526ebea1d578f5a66aa91515bccf2299
TheCarrotCutter <masonthedecker@gmail.com>
2024-11-13 10:05:13 -0500

    Update script.js

--- api/static/script.js
+++ api/static/script.js
@@ -2,6 +2,7 @@
 let number = parseInt(localStorage.getItem('number_save')) || 0;
 let increment_ammount = parseInt(localStorage.getItem('increment_ammount_save')) || 1;
 let total = parseInt(localStorage.getItem('total_save')) || 0;
+let percentages_shown = 0;
 
 // Starting price of buttons, default to 99 + increment_ammount
 let price = 99 + increment_ammount;
