c31e10eb2f6bc825c5a7e6dc07b2298bb6ed2cdf
TheCarrotCutter <masonthedecker@gmail.com>
2024-12-13 09:32:13 -0500

    Update script.js

--- api/static/script.js
+++ api/static/script.js
@@ -34,10 +34,12 @@ function sleep(ms) {
 function debug_box_open() {
     if (document.getElementById('myTextBox').value === "debug_on") {
         visible_items.push('debug_box');
+        console.log("yes")
         update();
     }
     if (document.getElementById('myTextBox').value === "debug_off") {
         visible_items = visible_items.filter(item => item !== 'debug_box');
+        console.log("yes")
         update();
     }
 }
